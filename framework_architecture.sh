d839f94828f735e5db6bc76eca8b814acc438b4d
YongSun Kim <sunny@gomfactory.com>
2021-02-05 15:00:59 +0900

    [Modify] add shell script for framework architecture validation

---
+++ framework_architecture.sh
@@ -0,0 +1,5 @@
+lipo -info ios/ADXLibrary-Cauly.framework/ADXLibrary-Cauly
+lipo -info ios/ADXLibrary-FBAudienceNetwork.framework/ADXLibrary-FBAudienceNetwork
+lipo -info ios/ADXLibrary-UnityAds.framework/ADXLibrary-UnityAds
+lipo -info ios/ADXLibrary-Vungle.framework/ADXLibrary-Vungle
+lipo -info ios/ADXLibrary.framework/ADXLibrary
