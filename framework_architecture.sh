a5c3492a297fb07481b1e9a08285664d50025ff6
hnroh <hnroh@adxcorp.kr>
2021-04-13 12:26:34 +0900

    [수정] framework_architecture.sh에 Pangle 프레임워크 추가

--- framework_architecture.sh
+++ framework_architecture.sh
@@ -1,5 +1,6 @@
 lipo -info ios/ADXLibrary-Cauly.framework/ADXLibrary-Cauly
 lipo -info ios/ADXLibrary-FBAudienceNetwork.framework/ADXLibrary-FBAudienceNetwork
+lipo -info ios/ADXLibrary-Pangle.framework/ADXLibrary-Pangle
 lipo -info ios/ADXLibrary-UnityAds.framework/ADXLibrary-UnityAds
 lipo -info ios/ADXLibrary-Vungle.framework/ADXLibrary-Vungle
 lipo -info ios/ADXLibrary.framework/ADXLibrary
