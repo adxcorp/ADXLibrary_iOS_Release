456dd4fc8ea97de4a137bd876a9d06e3b8627d86
hnroh <hnroh@adxcorp.kr>
2021-07-01 14:16:02 +0900

    [추가] ADXLibrary-Tapjoy 추가 (1.9.1)

---
+++ ios/ADXLibrary-Tapjoy.framework/Headers/ADXLibrary_Tapjoy.h
@@ -0,0 +1,18 @@
+//
+//  ADXLibrary_Tapjoy.h
+//  ADXLibrary-Tapjoy
+//
+//  Created by hnroh on 2021/06/28.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ADXLibrary_Tapjoy.
+FOUNDATION_EXPORT double ADXLibrary_TapjoyVersionNumber;
+
+//! Project version string for ADXLibrary_Tapjoy.
+FOUNDATION_EXPORT const unsigned char ADXLibrary_TapjoyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ADXLibrary_Tapjoy/PublicHeader.h>
+
+
