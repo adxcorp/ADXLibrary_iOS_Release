aaf5e583082e50f836f668366a753996781c232a
hnroh <hnroh@adxcorp.kr>
2021-07-29 17:21:17 +0900

    [수정] Framework 업데이트 (1.9.2)

--- ios/ADXLibrary.framework/Headers/ADXGDPR.h
+++ ios/ADXLibrary.framework/Headers/ADXGDPR.h
@@ -8,7 +8,7 @@
 #import <Foundation/Foundation.h>
 #import "ADXConsentViewController.h"
 
-#define ADX_SDK_VERSION @"1.9.1"
+#define ADX_SDK_VERSION @"1.9.2"
 
 typedef NS_ENUM(NSInteger, ADXConsentState) {
     ADXConsentStateUnknown      = 0,
