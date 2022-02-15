2b27d1f46c74443bc026e77550203a8f390bbd32
hnroh <hnroh@adxcorp.kr>
2022-02-15 14:44:52 +0900

    [수정] update.sh 수정

--- update.sh
+++ update.sh
@@ -1,5 +1,4 @@
 pod repo push ADXLibrary ADXLibrary-UnityAds.podspec --allow-warnings --verbose --skip-import-validation --use-libraries
-pod repo push ADXLibrary ADXLibrary-Vungle.podspec --allow-warnings --verbose --skip-import-validation --use-libraries
 pod repo push ADXLibrary ADXLibrary-FBAudienceNetwork.podspec --allow-warnings --verbose --skip-import-validation --use-libraries
 pod repo push ADXLibrary ADXLibrary-Fyber.podspec --allow-warnings --verbose --skip-import-validation --use-libraries
 pod repo push ADXLibrary ADXLibrary-Cauly.podspec --allow-warnings --verbose --skip-import-validation --use-libraries
