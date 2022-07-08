e612b7ed107b3ce5bbeda742d10b293cbed37f61
hnroh <hnroh@adxcorp.kr>
2022-07-08 20:10:21 +0900

    [수정] framework_architecture.sh 수정

--- framework_architecture.sh
+++ framework_architecture.sh
@@ -1,6 +1,7 @@
 lipo -info ios/ADXLibrary.framework/ADXLibrary
 lipo -info ios/ADXLibrary-Cauly.framework/ADXLibrary-Cauly
 lipo -info ios/ADXLibrary-FBAudienceNetwork.framework/ADXLibrary-FBAudienceNetwork
+lipo -info ios/ADXLibrary-Fyber.framework/ADXLibrary-Fyber
 lipo -info ios/ADXLibrary-Pangle.framework/ADXLibrary-Pangle
 lipo -info ios/ADXLibrary-Tapjoy.framework/ADXLibrary-Tapjoy
 lipo -info ios/ADXLibrary-UnityAds.framework/ADXLibrary-UnityAds
