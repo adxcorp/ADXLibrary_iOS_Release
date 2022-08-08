139db1af579c11f1f533e0d0ccb7ceb06dc4ed29
hnroh <hnroh@adxcorp.kr>
2022-08-08 14:50:43 +0900

    [수정] update.sh 수정

--- update.sh
+++ update.sh
@@ -4,5 +4,6 @@ pod repo push ADXLibrary ADXLibrary-Fyber.podspec --allow-warnings --verbose --s
 pod repo push ADXLibrary ADXLibrary-Cauly.podspec --allow-warnings --verbose --skip-import-validation --use-libraries
 pod repo push ADXLibrary ADXLibrary-Pangle.podspec --allow-warnings --verbose --skip-import-validation --use-libraries
 pod repo push ADXLibrary ADXLibrary-Tapjoy.podspec --allow-warnings --verbose --skip-import-validation --use-libraries
+pod repo update
 pod spec lint ADXLibrary.podspec --sources='https://github.com/adxcorp/ADXLibrary_iOS_Release.git, https://github.com/CocoaPods/Specs.git'  --allow-warnings --verbose --skip-import-validation --use-libraries
 pod repo push ADXLibrary ADXLibrary.podspec --allow-warnings --verbose --skip-import-validation --use-libraries
