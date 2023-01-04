b24062cb15030350735072c176b15920ce9f3512
YongSun Kim <sunny@adxcorp.kr>
2023-01-04 16:00:11 +0900

    [수정] framework_architecture.sh 수정

--- framework_architecture.sh
+++ framework_architecture.sh
@@ -1,7 +1,9 @@
 lipo -info ios/ADXLibrary.framework/ADXLibrary
 lipo -info ios/ADXLibrary-Cauly.framework/ADXLibrary-Cauly
+lipo -info ios/ADXLibrary-Criteo.framework/ADXLibrary-Criteo
 lipo -info ios/ADXLibrary-FBAudienceNetwork.framework/ADXLibrary-FBAudienceNetwork
 lipo -info ios/ADXLibrary-Fyber.framework/ADXLibrary-Fyber
+lipo -info ios/ADXLibrary-Mintegral.framework/ADXLibrary-Mintegral
 lipo -info ios/ADXLibrary-Pangle.framework/ADXLibrary-Pangle
 lipo -info ios/ADXLibrary-Tapjoy.framework/ADXLibrary-Tapjoy
 lipo -info ios/ADXLibrary-UnityAds.framework/ADXLibrary-UnityAds
