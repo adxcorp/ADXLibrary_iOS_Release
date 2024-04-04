adfa3f17726da0b7a216bd97fbd74ea605f98202
jclee <jclee@adxcorp.kr>
2024-04-04 14:54:45 +0900

    [수정] ADXLibrary 업데이트 (2.5.4)

--- update.sh
+++ update.sh
@@ -1,4 +1,3 @@
-pod repo push ADXLibrary ADXLibrary-Cauly.podspec --allow-warnings --verbose --skip-import-validation --use-libraries
 pod repo push ADXLibrary ADXLibrary-FBAudienceNetwork.podspec --allow-warnings --verbose --skip-import-validation --use-libraries
 pod repo push ADXLibrary ADXLibrary-Fyber.podspec --allow-warnings --verbose --skip-import-validation --use-libraries
 pod repo push ADXLibrary ADXLibrary-Mintegral.podspec --allow-warnings --verbose --skip-import-validation --use-libraries
